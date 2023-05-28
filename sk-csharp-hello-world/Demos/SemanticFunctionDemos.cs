79182d0566610eb60ff828874131a029330ed928
David Barkol <davidbarkol@ZohanMacbook.local>
2023-05-28 15:48:47 -0700

    initial revision

---
+++ sk-csharp-hello-world/Demos/SemanticFunctionDemos.cs
@@ -0,0 +1,75 @@
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace Demos
+{
+    public static class SemanticFunctionDemos
+    {
+        public static async Task CallSemanticFunction(IKernel kernel)
+        {
+            // Find the folder with all the skills/plugins
+            var skillsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+
+            // Import the plugin
+            // NOTE: A plugin can have many functions
+            var plugIn = kernel.ImportSemanticSkillFromDirectory(skillsDirectory,       // Path
+                                                        "SemanticPlugin");              // Plugin name
+
+            // Add variables to a context
+            // These variables will be passed into the function
+            var context = new ContextVariables();
+            context.Set("input", "stock market");
+            context.Set("style", "yoda");
+
+            // Call the semantic function
+            // var jokeFunctionName = "Joke";
+            // var result = await kernel.RunAsync(context, plugIn[jokeFunctionName]);
+            // Console.WriteLine(result);
+
+            // Chain example
+            //var excusesFunctionName = "Excuses";
+            var chuckNorrisFunctionName = "ChuckNorris";
+            var chainContext = new ContextVariables();
+            chainContext.Set("input", "karate");
+            ISKFunction[] pipeline = {
+                  //plugIn[excusesFunctionName],
+                  plugIn[chuckNorrisFunctionName]
+            };
+            var chainResult = await kernel.RunAsync(chainContext, pipeline);
+            Console.WriteLine(chainResult);
+        }
+
+        public static async Task BasicChaining(IKernel kernel)
+        {
+            string myJokePrompt = """
+            Tell a short joke about {{$INPUT}}.
+            """;
+
+            string myPoemPrompt = """
+            Take this "{{$INPUT}}" and convert it to a nursery rhyme.
+            """;
+
+            string myMenuPrompt = """
+            Make this poem "{{$INPUT}}" influence the three items in a coffee shop menu. 
+            The menu reads in enumerated form:
+            1.
+            """;
+
+            // Create inline semantic functions
+            var myJokeFunction = kernel.CreateSemanticFunction(myJokePrompt, maxTokens: 500);
+            var myPoemFunction = kernel.CreateSemanticFunction(myPoemPrompt, maxTokens: 500);
+            var myMenuFunction = kernel.CreateSemanticFunction(myMenuPrompt, maxTokens: 500);
+
+            // Chain the functions together and pass in a context
+            var myOutput = await kernel.RunAsync(
+                new ContextVariables("Charlie Brown"),
+                myJokeFunction,
+                myPoemFunction,
+                myMenuFunction);
+
+            Console.WriteLine(myOutput);
+        }
+
+    }
+}
---
+++ sk-csharp-hello-world/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+
+// Load kernel configuration settings
+var kernelSettings = KernelSettings.LoadSettings();
+var kernelConfig = new KernelConfig();
+
+// Add completion model
+kernelConfig.AddCompletionBackend(kernelSettings);
+
+// Initialize the logger
+using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .SetMinimumLevel(kernelSettings.LogLevel ?? LogLevel.Warning)
+        .AddConsole()
+        .AddDebug();
+});
+
+// Create the kernel
+IKernel kernel = new KernelBuilder().WithLogger(loggerFactory.CreateLogger<IKernel>()).WithConfiguration(kernelConfig).Build();
+
+// Demo: Call a semantic function from a plugin
+await Demos.SemanticFunctionDemos.CallSemanticFunction(kernel);
+
+// Demo: Chain semantic functions together
+//await Demos.SemanticFunctionDemos.BasicChaining(kernel);
